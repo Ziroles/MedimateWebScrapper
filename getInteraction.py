70444058d8385172dfe2a573e7be36adbb65be15
root <root@debian-python>
2024-02-14 11:31:51 +0000

    Webscrapper for medicine interaction and resume

---
+++ getInteraction.py
@@ -0,0 +1,47 @@
+import requests
+
+import json
+from tqdm import tqdm  
+from bs4 import BeautifulSoup
+
+tab_dict = {}
+
+def gethtmlContent(url):
+    response = requests.get(url)
+    return response.text
+
+def htmlParser(content):
+    soup = BeautifulSoup(content, 'html.parser')
+    res = ""
+    for link in soup.find_all('p'):
+        res += link.text + "\n"
+    return res
+
+def getContre_indications(content):
+    if "4.5. Interactions avec d'autres médicaments et autres formes d'interactions" not in content:
+        return ""
+    return content.split("4.5. Interactions avec d'autres médicaments et autres formes d'interactions")[1].split("4.6")[0]
+
+def main(code_cis):
+    url = 'https://base-donnees-publique.medicaments.gouv.fr/affichageDoc.php?specid={}&typedoc=R'.format(code_cis)
+    content = gethtmlContent(url)
+    
+    tab_dict[code_cis] = getContre_indications(htmlParser(content))
+
+
+
+with open('medicines_flat.json', 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+codes_cis = [obj["code_cis"] for obj in data]
+
+
+for code in tqdm(codes_cis):
+    main(code)
+
+
+with open('interaction.json', 'w', encoding='utf-8') as f:
+    json.dump(tab_dict, f, ensure_ascii=False, indent=4)
+
+print("Exportation terminée. Le fichier 'sideEffect.json' contient les sides effects des médicaments.")
+
---
+++ labelisation.py
@@ -0,0 +1,29 @@
+import json
+from tqdm import tqdm
+
+with open('sideEffect.json', 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+
+classifier =["Enfant","enceintes","enceinte","allaitement","nouveau-né","nourrisson","allaité"]
+
+allergie = ["blé","tiaprofénique","AINS", "acide acétylsalicylique","constituant","anesthésiques","cromoglicate de sodium","naproxène","sodique", "soja", "céphalosporines","salicylés","chlorhexidine"
+            "ibuprofène","antihistaminiques","Astéracées","dhuile darachide","photosensibilité","céthexonium","arachine","carboplatine","céfuroxime","lysozyme","excipients","macrolides","foins","urticaire","un des composants","aspirine","anti-inflammatoires non stéroïdiens","hexamidine","tétracyclines",
+            "angiœdème","angio-dème","rhinite aiguë","diclofénac","paracétamol","maïs","fénofibrate","kétoprofène","piroxicam","fibrates","atopiques","lactose","formotérol","furosémide","sulfonamides","sulfamides","viande rouge","abats","escargots","tropomyosine","prostaglandines","héparines","héparinoïdes"
+            "ammoniums","indométacine","lévocétirizine","cétirizine","hydroxyzine","pipérazine","sulfamides","chélates","mupirocine","polyéthylène","glycol","macrogol","niflumique","morniflumate",
+            "thrombocytes","flurbiprofène","phénoxyméthylpénicilline","pénicillines","théophylline","méfénamique","benzocaïne","atropine","ipratropium","oxitropium"
+            "tiotropium","formaldéhyde","néomycine","gentamicine","protamine","chlorhydrate","amide","sulfite","salbutamol","monohydraté","cobalamines","parahydroxybenzoates","parabens","méthylparaben","propylparaben","butylparaben","éthylparaben","isobutylparaben","isopropylparaben"
+            ]
+
+
+tab  ={}
+
+for obj in tqdm(data.keys()):
+    
+    tab[obj] = {"Warning":[e for e in data[obj].split(" ") if e in classifier],"Allergie":[e for e in data[obj].split(" ") if e in allergie],"Content":data[obj].replace("  \n","")}
+    
+print(tab)
+with open("tab.json", 'w') as file:
+    json.dump(tab, file, ensure_ascii=False, indent=4)
+
+
